e40658ae9390a9e5d60eb4ee5bdfdf20295f8248
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-27 11:48:25 +0530

    Add project files.

---
+++ FormatResult/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormatResult.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
